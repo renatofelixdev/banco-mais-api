dad1bdf2bba2a28d3f36a80ef54b17915c4353c3
Renato Felix <renatofelixss@hotmail.com>
2018-04-29 20:45:47 -0300

    rotas para operacoes bancarias

---
+++ app/api/BankingOperationApiController.java
@@ -0,0 +1,20 @@
+package api;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+
+public class BankingOperationApiController extends Controller {
+
+    public Result bankStatement(){
+        return null;
+    }
+    public Result deposit(){
+        return null;
+    }
+    public Result bankTransfer(){
+        return null;
+    }
+    public Result accountWithDrawal(){
+        return null;
+    }
+}
