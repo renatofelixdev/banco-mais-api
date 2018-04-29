fe36dc81d2b3d131d37438a7ba1370aaaa37ed36
Renato Felix <renatofelixss@hotmail.com>
2018-04-29 20:00:10 -0300

    rotas para crud usuario

---
+++ app/api/UserClientApiController.java
@@ -0,0 +1,36 @@
+package api;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+
+public class UserClientApiController extends Controller implements ApiController {
+    @Override
+    public Result all() {
+        return null;
+    }
+
+    @Override
+    public Result byId(Long id) {
+        return null;
+    }
+
+    @Override
+    public Result save() {
+        return null;
+    }
+
+    @Override
+    public Result update(Long id) {
+        return null;
+    }
+
+    @Override
+    public Result delete(Long id) {
+        return null;
+    }
+
+    @Override
+    public Result alterStatus(Long id) {
+        return null;
+    }
+}
